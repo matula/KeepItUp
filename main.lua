3aacef125a506619681bee20344839b3b244ada4
terry <terry@perk.com>
2016-06-11 13:30:59 -0500

    remove wav files

--- main.lua
+++ main.lua
@@ -6,3 +6,9 @@ background = display.newRect(_centerX, _centerY, _width, _height + 100)
 
 local composer = require("composer")
 composer.gotoScene("title")
+
+--[[
+ideas
+* move platform up and down after a while
+* modes - "endurance" = no enemies, "challenge" = with enemies
+ ]]
